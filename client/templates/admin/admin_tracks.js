2c4951bde1c32a3da4f2c5675ad96b1a9eb8558e
Rens Holmer <rens.holmer@wur.nl>
2016-10-18 12:58:38 +0200

    rename

--- client/templates/admin/admin_tracks.js
+++ client/templates/admin/admin_tracks.js
@@ -1,15 +1,12 @@
 Template.adminTracks.helpers({
 	hasDb: function(){
-		console.log(this.blastdbs);
 		const hasDb = this.blastdbs !== undefined 
-		console.log(hasDb)
 		return hasDb
 	}
 })
 
 Template.adminTracks.events({
 	'click .makeblastdb':function(){
-		console.log(this.track)
 		const track = this.track;
 		Meteor.call('makeBlastDb',track,function(error,result){
 			if (error){
