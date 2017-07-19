afe19b2e70aca47fab4688dcaf2a3a1ecec6042d
Rens Holmer <rens.holmer@wur.nl>
2017-07-19 13:29:29 +0200

    meteor version 1.6-beta9

--- server/accounts.js
+++ server/accounts.js
@@ -8,4 +8,6 @@ Accounts.onLogout(function(options){
 	Meteor.users.update({_id:options.user._id,'presence.status':'online'},{$set:{'presence.status':'offline'}})
 	Genes.update({'viewing':options.user._id},{$pull:{'viewing':options.user._id}})
 	Genes.update({'viewing':{$exists:true,$size:0}},{$unset:{'viewing':1}})
+  //Since we are on the server, the following does not work. Need to design a 'loggedIn' template / high order component
+  FlowRouter.redirect('/login')
 })
