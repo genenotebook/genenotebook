fb00a1f1cdc6a95ecffc5319074671def2184bba
Rens Holmer <rens.holmer@wur.nl>
2017-08-25 09:03:01 +0200

    react userprofile page

--- imports/ui/pages/user-profile/user-profile.js
+++ imports/ui/pages/user-profile/user-profile.js
@@ -1,10 +1,10 @@
 import { Template } from 'meteor/templating';
 
 import './user-profile.html';
-import userProfile from './user-profile.jsx';
+import UserProfileContainer from './user-profile.jsx';
 
 Template.userProfile.helpers({
-  userProfile(){
-    return userProfile;
+  UserProfileContainer(){
+    return UserProfileContainer;
   }
 })
--- imports/ui/pages/user-profile/user-profile.jsx
+++ imports/ui/pages/user-profile/user-profile.jsx
@@ -1,12 +1,43 @@
+import { Meteor } from 'meteor/meteor';
+import { createContainer } from 'meteor/react-meteor-data';
+
 import React from 'react';
 
-export default class userProfile extends React.Component {
+class UserProfile extends React.Component {
   constructor(props) {
     super(props);
   };
   render(){
+    const user = this.props.user;
+
+    console.log(user)
+
+    if (user === undefined){
+      return <div className="user-profile">Loading</div>
+    }
     return (
-      <p>This is the userprofile</p>
+      <div className="user-profile">
+        <h4>User profile</h4>
+        <ul>
+          <li>Username: {user.username}</li>
+          <li>First name: {user.profile.first_name}</li>
+          <li>Last name: {user.profile.last_name}</li>
+          <li>Emails:
+            <ul>
+              {user.emails.map( email => {
+                return <li key={email.address}>{email.address} ({email.verified ? 'verified' : 'unverified'})</li>
+              })}
+            </ul> 
+          </li>
+          <li>Groups: {user.roles}</li>
+        </ul>
+      </div>
     )
   }
 }
+
+export default UserProfileContainer = createContainer(() => {
+ return {
+  user: Meteor.user()
+ }
+},UserProfile)
