47ef385552d382c87162086ecc633c214bd61ab5
Rens Holmer <rens.holmer@wur.nl>
2018-07-31 16:38:12 +0200

    bioconda build

--- bundle.sh
+++ bundle.sh
@@ -1,23 +1,22 @@
 #!/usr/bin/env bash
 
-#set -o errexit
-set -o pipefail
-#set -o nounset
-set -o xtrace
+set -eo pipefail
+
+cat $0
 
 VERSION=$(jq -r '.version' package.json)
 
 BUNDLE_NAME="genenotebook_v$VERSION"
 
-meteor build --directory --allow-superuser $BUNDLE_NAME &&\
-mv $BUNDLE_NAME/bundle/* $BUNDLE_NAME &&\
-pushd $BUNDLE_NAME/programs/server &&\
-npm install &&\
-popd &&\
-pushd scripts &&\
-npm install &&\
-popd &&\
-cp -r scripts/* $BUNDLE_NAME &&\
-cp -r testdata $BUNDLE_NAME &&\
-cp -r LICENSE $BUNDLE_NAME #&&\
+meteor build --directory --allow-superuser $BUNDLE_NAME 
+mv $BUNDLE_NAME/bundle/* $BUNDLE_NAME 
+pushd $BUNDLE_NAME/programs/server 
+npm install 
+popd 
+pushd scripts 
+npm install 
+popd 
+cp -r scripts/* $BUNDLE_NAME 
+cp -r testdata $BUNDLE_NAME 
+cp -r LICENSE $BUNDLE_NAME #
 #tar cvzf $BUNDLE_NAME.tgz $BUNDLE_NAME 
