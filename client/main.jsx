332b728433836cf34567d09b1795c046b257dd28
Rens Holmer <rens.holmer@wur.nl>
2018-11-28 15:36:11 +0100

    remove setBaseUrl import

--- client/main.jsx
+++ client/main.jsx
@@ -4,8 +4,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import WebFont from 'webfontloader';
 
-import { setBaseUrl } from '/imports/api/methods/setBaseUrl.js';
-
 import App from '/imports/ui/main/App.jsx';
 
 import '/imports/ui/global_stylesheets/global.scss';
@@ -17,7 +15,5 @@ WebFont.load({
 });
 
 Meteor.startup(() => {
-  setBaseUrl.call();
-  console.log(Meteor)
   render(<App />, document.getElementById('genenotebook-root'));
 });
