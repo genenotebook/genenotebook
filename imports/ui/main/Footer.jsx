297958ccb2a6c850398cccb04fb406af4865fb4d
Rens Holmer <rens.holmer@wur.nl>
2019-03-21 15:21:37 +0100

    footer

--- imports/ui/main/Footer.jsx
+++ imports/ui/main/Footer.jsx
@@ -1,7 +1,10 @@
+import { Meteor } from 'meteor/meteor';
+
 import React, { useState } from 'react';
 import getVersion from '/imports/api/methods/getVersion.js';
 
 export default function Footer() {
+  const urlPrefix = Meteor.absoluteUrl();
   const [version, setVersion] = useState('...');
   getVersion.call({}, (err, res) => {
     if (err) console.error(err);
@@ -10,12 +13,31 @@ export default function Footer() {
   return (
     <footer className="footer py-1 my-auto bg-light border">
       <div className="container">
-        <span className="text-muted">
+        <a
+          href="https://genenotebook.github.io/"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{
+            color: '#A9A9A9',
+          }}
+        >
           <small>
+            <img
+              src={`${urlPrefix}logo_greyscale.svg`}
+              alt="GeneNoteBook logo"
+              className="footer-logo rounded-circle border"
+              style={{
+                width: '26px',
+                marginTop: '0px',
+                marginRight: '5px',
+              }}
+            />
+            <em>
 GeneNoteBook v
-            {version}
+              {version}
+            </em>
           </small>
-        </span>
+        </a>
       </div>
     </footer>
   );
