6684ea92c9c31089123b79eb22fb8f51b69b1643
Holmer, Rens <rens.holmer@wur.nl>
2019-09-13 13:14:17 +0200

    try to fix CORS issue with fontello

--- imports/startup/server/dynamic-head.js
+++ imports/startup/server/dynamic-head.js
@@ -5,7 +5,9 @@ WebApp.connectHandlers.use(
   Meteor.bindEnvironment((req, res, next) => {
     req.dynamicHead = req.dynamicHead || '';
     ['fontello.css', 'animation.css'].forEach((file) => {
-      req.dynamicHead += `<link rel='stylesheet' type='text/css' href='${Meteor.absoluteUrl()}fontello/css/${file}'>`;
+      req.dynamicHead += `<link rel='stylesheet' type='text/css' 
+                          href='${Meteor.absoluteUrl()}fontello/css/${file}'
+                          crossorigin='anonymous'>`;
     });
     next();
   }),
