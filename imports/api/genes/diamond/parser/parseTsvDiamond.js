e3db2f78e3f540342e951fb08df47c6e72ce426c
zygnematophyce <lfourteau91@gmail.com>
2022-05-19 14:57:49 +0200

    Parser tsv/tabular + add in diamond collection.

--- imports/api/genes/diamond/parser/parseTsvDiamond.js
+++ imports/api/genes/diamond/parser/parseTsvDiamond.js
@@ -7,7 +7,7 @@ class DiamondTsvProcessor {
     this.genesDb = Genes.rawCollection();
   }
 
-  parse = (line) => {
+  parse = async (line) => {
     // Get all 12 defaults preconfigured fields.
     const [
       qseqid,
@@ -33,6 +33,50 @@ class DiamondTsvProcessor {
       'query-from': qstart,
       'query-to': qend,
     };
+
+    // If subfeatures is found in genes database (e.g: ID =
+    // MMUCEDO_000002-T1).
+    const subfeatureIsFound = await this.genesDb.findOne(
+      { 'subfeatures.ID': qseqid },
+    );
+
+    if (typeof subfeatureIsFound !== 'undefined' && subfeatureIsFound !== null) {
+      // Update or insert if no matching documents were found.
+      const documentDiamond = diamondCollection.upsert(
+        { iteration_query: qseqid }, // selector.
+        {
+          $set: // modifier.
+          {
+            iteration_query: qseqid,
+          },
+        },
+      );
+
+      // Update or create if no matching documents were found
+      if (typeof documentDiamond.insertedId !== 'undefined') {
+        diamondCollection.update(
+          { iteration_query: qseqid },
+          {
+            $push: {
+              iteration_hits: iterations,
+            },
+          },
+        );
+      } else {
+        // Diamond collection already exists.
+        const diamondId = diamondCollection.findOne({ 'iteration_query': qseqid })._id;
+        diamondCollection.update(
+          { _id: diamondId },
+          {
+            $addToSet: {
+              iteration_hits: iterations,
+            },
+          },
+        );
+      }
+    } else {
+      logger.warn(`Warning ! No sub-feature was found for ${qseqid}.`);
+    }
   };
 }
 
