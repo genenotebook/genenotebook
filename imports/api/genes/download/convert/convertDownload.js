68d74ab6568a14878072f93a8440e4c70265fb71
Zygnematophyce <39624027+Zygnematophyce@users.noreply.github.com>
2022-10-31 16:27:23 +0000

    Fix the download problem (#293)
    * Create orthogroup folder.
    
    * Add prefixe argument in cli orthogroups command.
    
    * Try to add somes parameters.
    
    * Add class for all prefixes functions of orthofinder.
    
    * Remove prefixes dones.
    
    * Add orthofinder prefixes + join orthogroups and genes collection + doc.
    
    * Add the orthogroups, done !
    
    * Fix space bug.
    
    * typo fix.
    
    * Fix the rare case of a single hit + Gene ID rather than the name of the
    sequence in aligment collection.
    
    * Fix UpperCamelCase typo + better find a similar sequence.
    
    * Add some documentation.
    
    * Fix key bug.
    
    * The download works properly.
    
    * File not required.
    
    * Remove old code and console.log.

---
+++ imports/api/genes/download/convert/convertDownload.js
@@ -0,0 +1,179 @@
+import { Transcriptomes, ExperimentInfo } from '/imports/api/transcriptomes/transcriptome_collection.js';
+import { getGeneSequences } from '/imports/api/util/util.js';
+import logger from '/imports/api/util/logger.js';
+
+/**
+ * From the information of a gene convert and return the information in raw text
+ * in the chosen format (e.g. gff3, fasta, tsv).
+ * @class
+ * @constructor
+ * @public
+ */
+class DownloadDataConversion {
+  /**
+   * Assemble the gene information to build a file in gff3 format.
+   * (Used in the Annotation view of the download module).
+   * https://github.com/The-Sequence-Ontology/Specifications/blob/master/gff3.md
+   * @function
+   * @param {Object} gene - The gene document from collection e.g. { 'ID' : 'Ciclev10030451m', ...}
+   * @returns {String} - return a gff3.
+   */
+  convertGff3Format = ({ gene }) => {
+    const {
+      seqid,
+      source,
+      start: geneStart,
+      end: geneEnd,
+      score: geneScore,
+      strand,
+      attributes: geneAttributes,
+      subfeatures,
+    } = gene;
+
+    /**
+     * For column 9 : "attributes" of a gff3 we return the concatenation of
+     * attributes in the tag=value format. (Multiple tag=value pairs are separated
+     * by semicolons).
+     * @function
+     * @inner
+     * @param attributes {Object} - Feature attributes e.g. {"Name": ["Ciclev10030451m"]};
+     * @returns {String} Return attributes to format tags=values : "Name":
+     * ["Ciclev10030451m"] becomes Name=Ciclev10030451m;
+     */
+    function setAttrToTagValueFormat(attributes) {
+      return Object.entries(attributes)
+        .map(([k, v]) => `${k}=${v}`)
+        .join(';');
+    }
+
+    /** Create the list in gff3 format for each subfeatures. */
+    const gffLines = subfeatures.map(
+      ({
+        type: subType,
+        start: subStart,
+        end: subEnd,
+        score: subScore,
+        phase: subPhase,
+        attributes: subAttributes,
+        parents,
+      }) => {
+        // eslint-disable-next-line no-param-reassign
+        subAttributes.Parent = parents;
+
+        /** Get gene attributes to tag=value format. */
+        const subAttributeString = setAttrToTagValueFormat(subAttributes);
+
+        /** Gather information to create a gff3 line. */
+        return `${seqid}\t${source}\t${subType}\t${subStart}\t${subEnd}\t${subScore}\t${strand}\t${subPhase}\t${subAttributeString}`;
+      },
+    );
+
+    /** Add to the beginning of the list the attributes of the gene. */
+    const geneAttributeString = setAttrToTagValueFormat(geneAttributes);
+    const geneLine = `${seqid}\t${source}\tgene\t${geneStart}\t${geneEnd}\t${geneScore}\t${strand}\t-\t${geneAttributeString}`;
+    gffLines.unshift(geneLine);
+
+    /** Convert array to string. */
+    return gffLines.join('\n');
+  };
+
+  /**
+   * Assemble the gene(s) sequences to build a file in fasta format.
+   * (Used in the Sequence view of the download module).
+   * @function
+   * @param {Object} gene - The gene document from collection e.g. { 'ID' : 'Ciclev10030451m', ...}.
+   * @param {Object} options - The options of the sequence part of the download module.
+   * @returns {String} Return a fasta file.
+   */
+  convertFastaFormat = ({ gene, options }) => {
+    const { seqType, primaryTranscriptOnly } = options;
+
+    /**
+     * Get nucleotide and protein sequences of all transcripts for a single gene
+     * (using CDS subfeatures).
+     */
+    const sequences = getGeneSequences(gene).sort((a, b) => b.nucl.length - a.nucl.length);
+
+    const slice = primaryTranscriptOnly ? 1 : sequences.length;
+    const fastaArray = sequences.slice(0, slice).map((seq) => {
+      const seqString = seq[seqType].match(/.{1,80}/g).join('\n');
+      return `>${seq.ID}\n${seqString}`;
+    });
+
+    /** Convert array to string. */
+    return fastaArray.join('\n');
+  };
+
+  /**
+   * Assemble the transcriptome abundances to build a file in tsv format.
+   * (Used in the Expression view of the download module).
+   * @function
+   * @param {Object} gene - The gene document from collection e.g. { 'ID' : 'Ciclev10030451m', ...}
+   * @param {Object} options - The options of the expression part (transcriptomes).
+   * @returns {String} - Return transcriptome abundance in tsv format.
+   */
+  AbundanceTsvFormat = ({ gene, options }) => {
+    const { ID: geneId } = gene;
+    const { selectedSamples } = options;
+    const line = selectedSamples.map((replicaGroup) => ExperimentInfo.find({ replicaGroup })
+      .fetch()
+      .map(({ _id }) => _id)
+      .sort()
+      .map((experimentId) => {
+        const { tpm = 'NA' } = Transcriptomes.findOne({
+          geneId,
+          experimentId,
+        }) || {};
+        return tpm;
+      })
+      .join('\t'));
+
+    line.unshift(geneId);
+    line.push('\n');
+
+    return line.join('\t');
+  };
+
+  /**
+   * Add header to the top of the file e.g. #gff3-version 3.
+   * @function
+   * @param {String} format - The type of conversion e.g. gff3, fasta, tsv.
+   * @param {String} options - The options is exists.
+   * @returns {String|null} The header.
+   */
+  getFormatHeader = ({ format, options }) => {
+    switch (format) {
+      case 'gff3':
+        return '##gff-version 3\n';
+      case 'fasta':
+        return null;
+      case 'tsv':
+        return `gene_id\t${options.selectedSamples.join('\t')}\n`;
+      default:
+        return null;
+    }
+  };
+
+  /**
+   * Select the type of conversion according to the format.
+   * @function
+   * @param {String} format - The type of conversion e.g. gff3, fasta, tsv.
+   * @param {Object} gene - The gene document from collection e.g. { 'ID' : 'Ciclev10030451m', ...}
+   * @param {Object} options - The options is exists.
+   * @returns {function} - Return the conversion result.
+   */
+  getConverter = ({ format, gene, options }) => {
+    switch (format) {
+      case 'gff3':
+        return this.convertGff3Format({ gene });
+      case 'fasta':
+        return this.convertFastaFormat({ gene, options });
+      case 'tsv':
+        return this.AbundanceTsvFormat({ gene, options });
+      default:
+        return gene.ID;
+    }
+  };
+}
+
+export default DownloadDataConversion;
--- imports/api/jobqueue/process-download.js
+++ imports/api/jobqueue/process-download.js
@@ -1,170 +1,79 @@
-import jobQueue from './jobqueue.js';
-
-import fs from 'fs';
-import zlib from 'zlib';
-
-import { getGeneSequences } from '/imports/api/util/util.js';
-import logger from '/imports/api/util/logger.js';
+import DownloadDataConversion from '/imports/api/genes/download/convert/convertDownload.js';
 import { Genes } from '/imports/api/genes/geneCollection.js';
-import {
-  Transcriptomes,
-  ExperimentInfo,
-} from '/imports/api/transcriptomes/transcriptome_collection.js';
+import logger from '/imports/api/util/logger.js';
+import jobQueue from './jobqueue.js';
+import zlib from 'zlib';
+import fs from 'fs';
 
 const FORMATS = {
-  Annotations: 'gff3',
-  Sequences: 'fasta',
+  Annotation: 'gff3',
+  Sequence: 'fasta',
   Expression: 'tsv',
 };
 
-function formatAttributes(attributes) {
-  return Object.entries(attributes)
-    .map(([k, v]) => `${k}=${v}`)
-    .join(';');
-}
+jobQueue.processJobs(
+  'download',
+  {
+    concurrency: 1,
+    payload: 1,
+  },
+  async (job, callback) => {
+    /** Get all parameters from the validMethod. */
+    const {
+      queryHash, // Md5 Hash e.g. e38b9766ac6220047212e761f32577cef7320e9f.
+      queryString, // Mongodb query e.g. {"ID":{"$in":["Ciclev10004109m.g.v1.0"]}}.
+      dataType, // View selectionned e.g. Annotation, Sequence, Expression.
+      options, // Some options e.g. { fileFormat: '.gff3', primaryTranscriptOnly: true }
+    } = job.data;
 
-function formatGff3({ gene, options }) {
-  // Options contains the attribute keys to include
-  const {
-    seqid,
-    source,
-    start: geneStart,
-    end: geneEnd,
-    score: geneScore,
-    strand,
-    attributes: geneAttributes,
-    subfeatures,
-  } = gene;
+    const downloadProcessor = new DownloadDataConversion();
 
-  const gffLines = subfeatures.map(
-    ({
-      type: subType,
-      start: subStart,
-      end: subEnd,
-      score: subScore,
-      phase: subPhase,
-      attributes: subAttributes,
-      parents,
-    }) => {
-      // eslint-disable-next-line no-param-reassign
-      subAttributes.Parent = parents;
-      const subAttributeString = formatAttributes(subAttributes);
-      return `${seqid}\t${source}\t${subType}\t${subStart}\t${subEnd}\t${subScore}\t${strand}\t${subPhase}\t${subAttributeString}`;
-    },
-  );
+    const query = JSON.parse(queryString);
 
-  const geneAttributeString = formatAttributes(geneAttributes);
-  const geneLine = `${seqid}\t${source}\tgene\t${geneStart}\t${geneEnd}\t${geneScore}\t${strand}\t-\t${geneAttributeString}`;
-  gffLines.unshift(geneLine);
+    /** The type of conversion chosen (depends on the view of the download). */
+    const format = FORMATS[dataType];
 
-  return gffLines.join('\n');
-}
+    const fileName = `GeneNoteBook_download_${queryHash}.${format}.gz`;
+    logger.log(`Preparing ${fileName} for download`);
 
-function formatTsv({ gene, options }) {
-  // Options contains the transcriptome samples to include
-  const { ID: geneId } = gene;
-  const { selectedSamples } = options;
-  const line = selectedSamples.map((replicaGroup) => ExperimentInfo.find({ replicaGroup })
-    .fetch()
-    .map(({ _id }) => _id)
-    .sort()
-    .map((experimentId) => {
-      const { tpm = 'NA' } = Transcriptomes.findOne({
-        geneId,
-        experimentId,
-      }) || {};
-      return tpm;
-    })
-    .join('\t'));
+    const gzip = zlib.createGzip();
+    gzip.pipe(fs.createWriteStream(fileName));
 
-  line.unshift(geneId);
-  line.push('\n');
+    gzip.on('error', async (err) => {
+      logger.error('Job fail to download !', err);
+      job.fail(err);
+      callback();
+    });
 
-  return line.join('\t');
-}
+    gzip.on('finish', async () => {
+      logger.log('Write stream is done');
+      job.done(fileName);
+      callback();
+    });
 
-function formatFasta({ gene, options }) {
-  const { seqType, primaryTranscriptOnly } = options;
-  const sequences = getGeneSequences(gene).sort((a, b) => b.nucl.length - a.nucl.length);
+    const querySize = Genes.find(query).count();
+    const stepSize = Math.round(querySize / 10);
 
-  const slice = primaryTranscriptOnly ? 1 : sequences.length;
-  const fastaArray = sequences.slice(0, slice).map((seq) => {
-    const seqString = seq[seqType].match(/.{1,80}/g).join('\n');
-    return `>${seq.ID}\n${seqString}`;
-  });
-  return fastaArray.join('\n');
-}
+    /** Add header to the top of the file e.g. #gff3-version 3 ... */
+    const header = downloadProcessor.getFormatHeader({ format, options });
+    if (header) gzip.write(header);
 
-function formatGene({ gene, format, options }) {
-  switch (format) {
-    case 'gff3':
-      return formatGff3({ gene, options });
-    case 'fasta':
-      return formatFasta({ gene, options });
-    case 'tsv':
-      return formatTsv({ gene, options });
-    default:
-      return gene.ID;
-  }
-}
+    /** For each selected gene(s) write the information in the gzip. */
+    const allGenes = await Genes.rawCollection().find(query).toArray();
+    allGenes.forEach((gene, index) => {
+      if (index % stepSize === 0) {
+        job.progress(index, querySize, { echo: true });
+      }
 
-function formatHeader({ format, options }) {
-  switch (format) {
-    case 'gff3':
-      return '##gff-version 3\n';
-    case 'fasta':
-      return null;
-    case 'tsv':
-      return `gene_id\t${options.selectedSamples.join('\t')}\n`;
-    default:
-      return null;
-  }
-}
+      /**
+        * Depending on the type of conversion returns the set in the correct
+        * format (gff3, fasta, tsv).
+        */
+      const lines = downloadProcessor.getConverter({ format, gene, options });
+      gzip.write(`${lines}\n`);
+    });
 
-function processDownload(job, callback) {
-  // logger.debug(job.data);
-  const {
-    queryHash, queryString, dataType, options,
-  } = job.data;
-  const query = JSON.parse(queryString);
-  const format = FORMATS[dataType];
-
-  const fileName = `GeneNoteBook_download_${queryHash}.${format}.gz`;
-
-  logger.log(`Preparing ${fileName} for download`);
-
-  const writeStream = fs.createWriteStream(fileName);
-  const compress = zlib.createGzip();
-  compress.pipe(writeStream);
-
-  // the finish event is emitted when all data has been flushed from the stream
-  compress.on('finish', async () => {
-    logger.log('wrote all data to file');
-    job.done(fileName);
-    callback();
-  });
-
-  const querySize = Genes.find(query).count();
-  const stepSize = Math.round(querySize / 10);
-
-  const header = formatHeader({ format, options });
-  if (header) compress.write(header);
-
-  Genes.find(query).forEach((gene, index) => {
-    if (index % stepSize === 0) {
-      job.progress(index, querySize, { echo: true });
-    }
-    const lines = formatGene({ gene, format, options });
-    compress.write(`${lines}\n`);
-  });
-
-  // close the stream
-  compress.end();
-}
-
-const options = {
-  concurrency: 1,
-  payload: 1,
-};
-
-jobQueue.processJobs('download', options, processDownload);
+    gzip.end();
+    logger.log('Compress gzip file sucess.');
+  },
+);
